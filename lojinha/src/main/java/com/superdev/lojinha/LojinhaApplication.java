1a4bde2efb5be042cad84498b4db3ca2d5730ca0
LUIZ ANTONIO MADRUGA <madruga_emo@hotmail.com>
2024-05-17 22:27:27 -0300

    :sparkles: feat(produto): CREATE and REAd para produto

--- lojinha/src/main/java/com/superdev/lojinha/LojinhaApplication.java
+++ lojinha/src/main/java/com/superdev/lojinha/LojinhaApplication.java
@@ -2,12 +2,14 @@ package com.superdev.lojinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.superdev.lojinha.controller")
 public class LojinhaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LojinhaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LojinhaApplication.class, args);
+    }
 
 }
